cc89701f97c401fb5e18a2de6af95fdce2c8f172
Vanshi <kvartech.vanshigala@gmail.com>
2026-04-07 09:47:43 +0530

    Rebuilding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,10 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
 
-import Dashboard from "./pages/Dashboard";
-import AddExpense from "./pages/AddExpense";
 
 function App() {
   return (
    
-      <Routes>
-        <Route path="/" element={<Login />} />
-     
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add" element={<AddExpense />} />
-      </Routes>
+      <>
+      <h1>Working</h1></>
    
   );
 }
